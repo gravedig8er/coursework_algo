3b180e78d22e87f4c86897bc509d932993b58c65
gravedig8er <froppee@mail.ru>
2025-12-09 22:18:04 +0300

    lst vlad

--- src/main/java/algo/Main.java
+++ src/main/java/algo/Main.java
@@ -5,5 +5,8 @@ public class Main {
         System.out.println("Pupok");
         System.out.println("сама такая");
         System.out.println("Hello");
+
+
+        System.out.println("asdasdasd");
     }
 }
