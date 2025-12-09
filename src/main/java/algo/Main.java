c96a0e91fb36684a5220c03288976a9f0b378abd
gravedig8er <froppee@mail.ru>
2025-12-09 22:06:14 +0300

    vlad commit

--- src/main/java/algo/Main.java
+++ src/main/java/algo/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Pupok");
         System.out.println("дамир лох");
+        System.out.println("asdasd");
     }
 }
