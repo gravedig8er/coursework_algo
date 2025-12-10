b74dd730b3ce25f0afc22f8c57d0085f922a766e
gravedig8er <froppee@mail.ru>
2025-12-10 12:06:09 +0300

    feat: entity

--- src/main/java/algo/Main.java
+++ src/main/java/algo/Main.java
@@ -6,12 +6,6 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        MyHashMap<String, Integer> map = new MyHashMap<>();
-        ArrayList<Integer> arrayList = new ArrayList<>();
-
-        arrayList.add(1);
-        System.out.println(arrayList.remove(Integer.valueOf(1)));
-
 
     }
 }
--- src/main/java/algo/map/IMap.java
+++ src/main/java/algo/map/IMap.java
@@ -1,5 +1,8 @@
 package algo.map;
 
+/*
+* СЮДА ПИСАТЬ ВСЕ ФУНКЦИИ
+* */
 public interface IMap<T, V> {
     void insert(T key, V value);
     V search(T key);
