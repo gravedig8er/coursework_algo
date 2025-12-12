4d31e13a003bba6048845982cb833abc8ac10bbc
gravedig8er <froppee@mail.ru>
2025-12-12 20:25:25 +0300

    feat: Iterator, delete, resize

--- src/main/java/algo/Main.java
+++ src/main/java/algo/Main.java
@@ -2,7 +2,6 @@ package algo;
 
 import algo.map.MyHashMap;
 
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/algo/map/IMap.java
+++ src/main/java/algo/map/IMap.java
@@ -4,7 +4,7 @@ package algo.map;
 * СЮДА ПИСАТЬ ВСЕ public ФУНКЦИИ
 *
 * */
-public interface IMap<K, V> {
+public interface IMap<K, V> extends Iterable<MyHashMap.Entity<K, V>> {
     void insert(K key, V value); // дамир
     V get(K key); // дамир
     boolean containsKey(K key); // дамир
