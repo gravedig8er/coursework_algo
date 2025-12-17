b37c80d88872a1ecd088d8f144a303142978796b
gravedig8er <froppee@mail.ru>
2025-12-17 22:31:26 +0300

    nothing

--- src/main/java/algo/Main.java
+++ src/main/java/algo/Main.java
@@ -7,8 +7,10 @@ public class Main {
     public static void main(String[] args) {
         MyHashMap<String, Integer> map = new MyHashMap<>();
 
-        map.insert("vlad", 1);
-        map.insert("damir", 2);
+        map.insert("арбуз", 1);
+        map.insert("дыня", 2);
+        map.insert("виноград", 3);
+        map.insert("клубника", 4);
 
         for (MyHashMap.Entity<String, Integer> stringIntegerEntity : map) {
             System.out.println(stringIntegerEntity.getKey() + " " + stringIntegerEntity.getValue());
